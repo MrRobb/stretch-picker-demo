a802c5a75d2be450e9d55d7871f7069df0837a44
Roberto Ariosa <roberto.ariosa@upc.edu>
2023-10-17 20:00:03 +0200

    Latest changes

--- src/smart_cities_clean/mover.py
+++ src/smart_cities_clean/mover.py
@@ -11,3 +11,45 @@ OUTPUT
 ------
 The module moves the robot gripper to the object.
 """
+
+import rospy
+from stretch_body.robot import Robot
+from geometry_msgs.msg import Point
+
+# Constants
+TABLE_HEIGHT = 0.75
+OBJECT_STRENGTH = {
+    "bottle": -50,
+    "remote": -50,
+}
+OBJECT_DEGREES = {
+    "bottle": -10,
+    "remote": -20,
+}
+
+# Globals
+r = Robot()
+
+
+if not r.startup():
+    exit()  # failed to start robot!
+
+# home the joints to find zero, if necessary
+if not r.is_calibrated():
+    r.home()
+
+
+def move_to(msg: Point):
+    """
+    This function implements the pick action server. It takes a string representing the object class and uses the robot to pick the object.
+    """
+    rospy.loginfo(f"Moving to: {msg}")
+
+
+if __name__ == "__main__":
+    # Initialize the ROS node.
+    rospy.loginfo("Starting pick node...")
+    rospy.init_node("mover")
+
+    # Create service server
+    mover = rospy.Subscriber("move_to", Point, move_to, queue_size=1)
--- src/smart_cities_clean/picker.py
+++ src/smart_cities_clean/picker.py
@@ -11,3 +11,141 @@ OUTPUT
 ------
 Sends the location of the object to the mover module.
 """
+
+from typing import Dict
+import rospy
+from picker_demo.srv import Picker, PickerRequest, PickerResponse
+from vision_msgs.msg import Detection2DArray, BoundingBox2D
+
+class_names = {
+    0: "person",
+    1: "bicycle",
+    2: "car",
+    3: "motorcycle",
+    4: "airplane",
+    5: "bus",
+    6: "train",
+    7: "truck",
+    8: "boat",
+    9: "traffic light",
+    10: "fire hydrant",
+    11: "stop sign",
+    12: "parking meter",
+    13: "bench",
+    14: "bird",
+    15: "cat",
+    16: "dog",
+    17: "horse",
+    18: "sheep",
+    19: "cow",
+    20: "elephant",
+    21: "bear",
+    22: "zebra",
+    23: "giraffe",
+    24: "backpack",
+    25: "umbrella",
+    26: "handbag",
+    27: "tie",
+    28: "suitcase",
+    29: "frisbee",
+    30: "skis",
+    31: "snowboard",
+    32: "sports ball",
+    33: "kite",
+    34: "baseball bat",
+    35: "baseball glove",
+    36: "skateboard",
+    37: "surfboard",
+    38: "tennis racket",
+    39: "bottle",
+    40: "wine glass",
+    41: "cup",
+    42: "fork",
+    43: "knife",
+    44: "spoon",
+    45: "bowl",
+    46: "banana",
+    47: "apple",
+    48: "sandwich",
+    49: "orange",
+    50: "broccoli",
+    51: "carrot",
+    52: "hot dog",
+    53: "pizza",
+    54: "donut",
+    55: "cake",
+    56: "chair",
+    57: "couch",
+    58: "potted plant",
+    59: "bed",
+    60: "dining table",
+    61: "toilet",
+    62: "tv",
+    63: "laptop",
+    64: "mouse",
+    65: "remote",
+    66: "keyboard",
+    67: "cell phone",
+    68: "microwave",
+    69: "oven",
+    70: "toaster",
+    71: "sink",
+    72: "refrigerator",
+    73: "book",
+    74: "clock",
+    75: "vase",
+    76: "scissors",
+    77: "teddy bear",
+    78: "hair drier",
+    79: "toothbrush",
+}
+
+# Constants
+DETECTIONS: Dict[str, BoundingBox2D] = {}
+
+
+def detections_callback(msg: Detection2DArray):
+    if msg.detections is None:
+        return
+
+    for detection in msg.detections:
+        if len(detection.results) > 0:
+            rospy.loginfo(f"Location: {detection.bbox}")
+            DETECTIONS[detection.results[0].id] = detection.bbox
+
+
+def pick(msg: PickerRequest) -> PickerResponse:
+    """
+    This function implements the pick action server. It takes a string representing the object class and uses the robot to pick the object.
+    """
+    rospy.loginfo(f"Picking: {msg.object_class}")
+
+    # Search for object in DETECTIONS
+    # If object not found, return error
+    if msg.object_class not in DETECTIONS:
+        return PickerResponse(False)
+
+    # If object found, call mover module
+    rospy.loginfo(f"Found: {msg.object_class}")
+    rospy.loginfo(f"Location: {DETECTIONS[msg.object_class]}")
+    
+
+    # Return success
+    return PickerResponse(True)
+
+
+if __name__ == "__main__":
+    # Initialize the ROS node.
+    rospy.loginfo("Starting pick node...")
+    rospy.init_node("picker")
+
+    # Create service server
+    rospy.loginfo("Creating detections subscriber...")
+    detections = rospy.Subscriber(
+        "/objects/detected", Detection2DArray, detections_callback, queue_size=1
+    )
+
+    rospy.loginfo("Creating service server...")
+    picker = rospy.Service("pick", Picker, pick)
+
+    rospy.spin()
